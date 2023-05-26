84118019e162acc28c5d135a390af27c2b553b16
kjyong702 <kjyong702@naver.com>
2023-05-26 12:22:17 +0900

    3001번 port로 변경

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'), { prefix: '/public' });
 
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
