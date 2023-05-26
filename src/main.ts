88e5ffa1e5dbdd00c41a2cf4f26ce00f091cbd96
kjyong702 <kjyong702@naver.com>
2023-05-26 20:32:22 +0900

    CORS 임시 허용

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.useStaticAssets(join(__dirname, '..', 'public'), { prefix: '/public' });
+  app.enableCors({
+    origin: '*',
+  });
 
   await app.listen(3001);
 }
