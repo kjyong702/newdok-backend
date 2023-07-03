13d67c71ca40735b282d87ebed6d2c9a3bbc46ca
kjyong702 <kjyong702@naver.com>
2023-07-03 15:21:59 +0900

    update: db 필드명 Camel Case로 전부 변경

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,10 +4,4 @@ import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
-
-  @Post('/signup')
-  async signup(@Body() body: any) {
-    // dto 작성 예정
-    return this.usersService.signup(body);
-  }
 }
