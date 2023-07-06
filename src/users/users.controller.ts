fc1d1e86b7301f3c2553291c20c1715f5eae99a6
kjyong702 <kjyong702@naver.com>
2023-07-06 19:37:26 +0900

    update: schema.prisma 최종 업데이트

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, Post, Body, Query, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
-
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+}
