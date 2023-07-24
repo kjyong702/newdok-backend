0816b1720059b2c3d9e7cf5e1f86c4090609d1da
kjyong702 <kjyong702@naver.com>
2023-07-24 16:42:49 +0900

    feat: 모든/추천 뉴스레터 기능 추가 및 뉴스레터 관련 기능 마무리

--- src/newsletters/newsletters.controller.ts
+++ src/newsletters/newsletters.controller.ts
@@ -12,26 +12,35 @@ export class NewslettersController {
     return this.newslettersService.getRecommendedNewsletters(req.user.id);
   }
 
+  @Get('/:id')
+  @UseGuards(AuthGuard)
+  async getNewsletterById(@Param('id') brandId: string, @Req() req: any) {
+    return this.newslettersService.getNewsletterById(brandId, req.user.id);
+  }
+
   @Get('')
+  @UseGuards(AuthGuard)
   async getAllNewsletters(
     @Query('orderOpt') orderOpt: string,
     @Query('industry') industries: string[],
     @Query('day') days: string[],
+    @Req() req: any,
   ) {
     return this.newslettersService.getAllNewsletters(
       orderOpt,
       industries,
       days,
+      req.user.id,
     );
   }
 
   @Get('/filtered/industry/:id')
-  async getNewslettersByIndustry(@Param('id') id: string) {
-    return this.newslettersService.getNewslettersByIndustry(id);
+  async getNewslettersByIndustry(@Param('id') industryId: string) {
+    return this.newslettersService.getNewslettersByIndustry(industryId);
   }
 
   @Get('/filtered/interest/:id')
-  async getNewslettersByInterest(@Param('id') id: string) {
-    return this.newslettersService.getNewslettersByInterest(id);
+  async getNewslettersByInterest(@Param('id') interestId: string) {
+    return this.newslettersService.getNewslettersByInterest(interestId);
   }
 }
--- src/newsletters/newsletters.service.ts
+++ src/newsletters/newsletters.service.ts
@@ -1,31 +1,227 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class NewslettersService {
   constructor(private prisma: PrismaService) {}
 
+  async getNewsletterById(brandId: string, userId: number) {
+    const newsletter = await this.prisma.newsletter.findUnique({
+      where: {
+        id: parseInt(brandId),
+      },
+      include: {
+        articles: {
+          take: 5,
+          distinct: ['title'],
+          select: {
+            id: true,
+            title: true,
+            date: true,
+          },
+        },
+      },
+    });
+    const isSubscribed = await this.prisma.newslettersOnUsers.findUnique({
+      where: {
+        userId_newsletterId: {
+          userId,
+          newsletterId: parseInt(brandId),
+        },
+      },
+    });
+    if (isSubscribed) {
+      return Object.assign(newsletter, { isSubsribed: 'CONFIRMED' });
+    } else {
+      return Object.assign(newsletter, { isSubscribed: 'INITIAL' });
+    }
+  }
+
+  async getAllNewsletters(
+    orderOpt: string,
+    industries: string[],
+    days: string[],
+    userId: number,
+  ) {
+    const industryIds = industries.map((industryId) => parseInt(industryId));
+    const dayIds = days.map((dayId) => parseInt(dayId));
+
+    // 1. 인기순 정렬 + 구독 중인 뉴스레터
+    if (orderOpt === '인기순') {
+      const arr1 = await this.prisma.newsletter.findMany({
+        where: {
+          AND: [
+            {
+              industries: {
+                some: { id: { in: industryIds } },
+              },
+            },
+            {
+              days: {
+                some: { id: { in: dayIds } },
+              },
+            },
+            {
+              users: {
+                some: { userId },
+              },
+            },
+          ],
+        },
+        orderBy: {
+          users: {
+            _count: 'desc',
+          },
+        },
+        include: {
+          interests: true,
+        },
+      });
+
+      const newslettersSubscribed = [];
+      arr1.forEach((newsletter) => {
+        newslettersSubscribed.push(
+          Object.assign(newsletter, { isSubsribed: 'CONFIRMED' }),
+        );
+      });
+      // 2. 인기순 정렬 + 구독 중이 아닌 뉴스레터
+      const arr2 = await this.prisma.newsletter.findMany({
+        where: {
+          AND: [
+            {
+              industries: {
+                some: { id: { in: industryIds } },
+              },
+            },
+            {
+              days: {
+                some: { id: { in: dayIds } },
+              },
+            },
+          ],
+          NOT: {
+            users: {
+              some: { userId },
+            },
+          },
+        },
+        orderBy: {
+          users: {
+            _count: 'desc',
+          },
+        },
+        include: {
+          interests: true,
+        },
+      });
+
+      const newslettersUnSubscribed = [];
+      arr2.forEach((newsletter) => {
+        newslettersUnSubscribed.push(
+          Object.assign(newsletter, { isSubsribed: 'INITIAL' }),
+        );
+      });
+
+      return newslettersSubscribed.concat(newslettersUnSubscribed);
+    } else {
+      // 3. 최신순 정렬 + 구독 중인 뉴스레터
+      const arr1 = await this.prisma.newsletter.findMany({
+        where: {
+          AND: [
+            {
+              industries: {
+                some: {
+                  id: { in: industryIds },
+                },
+              },
+            },
+            {
+              days: {
+                some: {
+                  id: { in: dayIds },
+                },
+              },
+            },
+            {
+              users: {
+                some: { userId },
+              },
+            },
+          ],
+        },
+        orderBy: {
+          createdAt: 'asc',
+        },
+        include: {
+          interests: true,
+        },
+      });
+
+      const newslettersSubscribed = [];
+      arr1.forEach((newsletter) => {
+        newslettersSubscribed.push(
+          Object.assign(newsletter, { isSubsribed: 'CONFIRMED' }),
+        );
+      });
+
+      // 4. 최신순 정렬 + 구독 중이 아닌 뉴스레터
+      const arr2 = await this.prisma.newsletter.findMany({
+        where: {
+          AND: [
+            {
+              industries: {
+                some: {
+                  id: { in: industryIds },
+                },
+              },
+            },
+            {
+              days: {
+                some: {
+                  id: { in: dayIds },
+                },
+              },
+            },
+          ],
+          NOT: {
+            users: {
+              some: { userId },
+            },
+          },
+        },
+        orderBy: {
+          createdAt: 'asc',
+        },
+        include: {
+          interests: true,
+        },
+      });
+
+      const newslettersUnSubscribed = [];
+      arr2.forEach((newsletter) => {
+        newslettersUnSubscribed.push(
+          Object.assign(newsletter, { isSubsribed: 'INITIAL' }),
+        );
+      });
+
+      return newslettersSubscribed.concat(newslettersUnSubscribed);
+    }
+  }
+
   async getRecommendedNewsletters(userId: number) {
     const user = await this.prisma.user.findUnique({
-      where: { id: userId },
-      include: { interests: true },
+      where: {
+        id: userId,
+      },
+      include: {
+        interests: true,
+      },
     });
 
-    const isSubscribedForIntersection = [];
-    const isSubscribedForUnion = [];
-
-    const subscribedNewsletters = await this.prisma.newslettersOnUsers.findMany(
-      {
-        where: { userId },
-      },
-    );
-
-    const subscribedNewsletterIds = [];
-    subscribedNewsletters.forEach((value: any) =>
-      subscribedNewsletterIds.push(value.newsletterId),
-    );
-
     const interestIds = user.interests.map((data) => data.interestId);
+    if (interestIds.length === 0) {
+      throw new BadRequestException('사전조사 미실시 유저입니다');
+    }
 
     const intersection = await this.prisma.newsletter.findMany({
       where: {
@@ -43,18 +239,16 @@ export class NewslettersService {
             },
           },
         ],
+        NOT: {
+          users: {
+            some: { userId },
+          },
+        },
       },
       include: {
         industries: true,
         interests: true,
       },
-    });
-    intersection.forEach((value: any) => {
-      if (!subscribedNewsletterIds.includes(value.id)) {
-        isSubscribedForIntersection.push(
-          Object.assign(value, { isSubscribed: '구독 전' }),
-        );
-      }
     });
 
     const union1 = await this.prisma.newsletter.findMany({
@@ -62,6 +256,11 @@ export class NewslettersService {
         industries: {
           some: {
             id: user.industryId,
+          },
+        },
+        NOT: {
+          users: {
+            some: { userId },
           },
         },
       },
@@ -73,6 +272,11 @@ export class NewslettersService {
         interests: {
           some: {
             id: { in: interestIds },
+          },
+        },
+        NOT: {
+          users: {
+            some: { userId },
           },
         },
       },
@@ -92,91 +296,8 @@ export class NewslettersService {
         interests: true,
       },
     });
-    union.forEach((value: any) => {
-      if (!subscribedNewsletterIds.includes(value.id)) {
-        isSubscribedForUnion.push(
-          Object.assign(value, { isSubscribed: '구독 전' }),
-        );
-      }
-    });
 
-    return {
-      intersection: isSubscribedForIntersection,
-      union: isSubscribedForUnion,
-    };
-  }
-
-  async getAllNewsletters(
-    orderOpt: string,
-    industries: string[],
-    days: string[],
-  ) {
-    const industryIds = industries.map((industryId) => parseInt(industryId));
-    const dayIds = days.map((dayId) => parseInt(dayId));
-
-    if (orderOpt === '인기순') {
-      const newsletters = await this.prisma.newsletter.findMany({
-        where: {
-          AND: [
-            {
-              industries: {
-                some: {
-                  id: { in: industryIds },
-                },
-              },
-            },
-            {
-              days: {
-                some: {
-                  id: { in: dayIds },
-                },
-              },
-            },
-          ],
-        },
-        orderBy: {
-          users: {
-            _count: 'desc',
-          },
-        },
-        include: {
-          industries: true,
-          interests: true,
-          days: true,
-        },
-      });
-      return newsletters;
-    } else {
-      const newsletters = await this.prisma.newsletter.findMany({
-        where: {
-          AND: [
-            {
-              industries: {
-                some: {
-                  id: { in: industryIds },
-                },
-              },
-            },
-            {
-              days: {
-                some: {
-                  id: { in: dayIds },
-                },
-              },
-            },
-          ],
-        },
-        orderBy: {
-          createdAt: 'asc',
-        },
-        include: {
-          industries: true,
-          interests: true,
-          days: true,
-        },
-      });
-      return newsletters;
-    }
+    return { intersection, union };
   }
 
   async getNewslettersByIndustry(id: string) {
