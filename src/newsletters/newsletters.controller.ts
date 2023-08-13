86222a81d9fa074c304bdd2953f943e44900b261
kjyong702 <kjyong702@naver.com>
2023-08-13 16:13:45 +0900

    feat: 비회원 뉴스레터 API 추가

--- src/newsletters/newsletters.controller.ts
+++ src/newsletters/newsletters.controller.ts
@@ -9,26 +9,12 @@ export class NewslettersController {
   constructor(private newslettersService: NewslettersService) {}
 
   @ApiOperation({
-    summary: '개인화 추천 뉴스레터',
+    summary: '개인화 추천 뉴스레터(회원)',
   })
   @Get('/recommend')
   @UseGuards(AuthGuard)
   async getRecommendedNewsletters(@Req() req: any) {
     return this.newslettersService.getRecommendedNewsletters(req.user.id);
-  }
-
-  @ApiParam({
-    name: 'id',
-    description: '뉴스레터 id',
-    example: 1,
-  })
-  @ApiOperation({
-    summary: '뉴스레터 브랜드 조회',
-  })
-  @Get('/:id')
-  @UseGuards(AuthGuard)
-  async getNewsletterById(@Param('id') brandId: string, @Req() req: any) {
-    return this.newslettersService.getNewsletterById(brandId, req.user.id);
   }
 
   @ApiQuery({
@@ -47,7 +33,7 @@ export class NewslettersController {
     example: 1,
   })
   @ApiOperation({
-    summary: '모든 뉴스레터 브랜드 조회',
+    summary: '모든 뉴스레터 브랜드 조회(회원)',
   })
   @Get('')
   @UseGuards(AuthGuard)
@@ -63,5 +49,43 @@ export class NewslettersController {
       days,
       req.user.id,
     );
+  }
+
+  @ApiOperation({
+    summary: '모든 뉴스레터 브랜드 조회(비회원)',
+  })
+  @Get('/non-member')
+  async getAllNewslettersForNonMember(
+    @Query('orderOpt') orderOpt: string,
+    @Query('industry') industries: string[],
+    @Query('day') days: string[],
+  ) {
+    return this.newslettersService.getAllNewslettersForNonMember(
+      orderOpt,
+      industries,
+      days,
+    );
+  }
+
+  @ApiParam({
+    name: 'id',
+    description: '뉴스레터 id',
+    example: 1,
+  })
+  @ApiOperation({
+    summary: '뉴스레터 브랜드 조회(회원)',
+  })
+  @Get('/:id')
+  @UseGuards(AuthGuard)
+  async getNewsletterById(@Param('id') brandId: string, @Req() req: any) {
+    return this.newslettersService.getNewsletterById(brandId, req.user.id);
+  }
+
+  @ApiOperation({
+    summary: '뉴스레터 브랜드 조회(비회원)',
+  })
+  @Get('/:id/non-member')
+  async getNewsletterByIdForNonMember(@Param('id') brandId: string) {
+    return this.newslettersService.getNewsletterByIdForNonMember(brandId);
   }
 }
