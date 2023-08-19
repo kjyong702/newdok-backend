191cf0a5636e3323cfbea2f524e7f897c3d6ac1f
kjyong702 <kjyong702@naver.com>
2023-08-19 16:21:16 +0900

    feat: POP3 자동화 요청을 위한  Task Scheduling 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScheduleModule } from '@nestjs/schedule';
 import { NewslettersModule } from './newsletters/newsletters.module';
 import { UsersModule } from './users/users.module';
 import { ArticlesModule } from './articles/articles.module';
 
 @Module({
-  imports: [NewslettersModule, UsersModule, ArticlesModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    NewslettersModule,
+    UsersModule,
+    ArticlesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
