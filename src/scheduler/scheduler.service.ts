8ddc80e6fd54201d2fa937218e9cef1027f0520d
kjyong702 <kjyong702@naver.com>
2023-10-09 17:46:30 +0900

    feat: AppModuel 의존성 제거 및 SchedulerModule 의존성 주입

--- src/scheduler/scheduler.service.ts
+++ src/scheduler/scheduler.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ArticlesService } from '../articles/articles.service';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
-export class SchedulerService {}
+export class SchedulerService {
+  constructor(
+    private articlesService: ArticlesService,
+    private prisma: PrismaService,
+  ) {}
+}
