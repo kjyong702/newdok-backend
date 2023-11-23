53751e4c2c9adf0d41acb621fe437d876b499f3d
kjyong702 <kjyong702@naver.com>
2023-11-23 10:51:18 +0900

    feat: 인증 로직을 담당하는 auth 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { UsersModule } from './users/users.module';
 import { NewslettersModule } from './newsletters/newsletters.module';
 import { ArticlesModule } from './articles/articles.module';
 import { SchedulerModule } from './scheduler/scheduler.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, NewslettersModule, ArticlesModule, SchedulerModule],
+  imports: [
+    UsersModule,
+    NewslettersModule,
+    ArticlesModule,
+    SchedulerModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
