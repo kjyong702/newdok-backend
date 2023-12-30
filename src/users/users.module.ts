5d583545d8aca28585ca14a4236f5d956dd55ff2
kjyong702 <kjyong702@naver.com>
2023-12-30 20:44:36 +0900

    feat: JwtModule 등록 시, 비동기 처리를 위해 registerAsync 함수 사용

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,6 +3,7 @@ import { NewslettersModule } from '../newsletters/newsletters.module';
 import { ArticlesModule } from '../articles/articles.module';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersController } from './users.controller';
+import { ConfigService } from '@nestjs/config';
 import { UsersService } from './users.service';
 import { PrismaService } from '../prisma.service';
 
@@ -10,10 +11,13 @@ import { PrismaService } from '../prisma.service';
   imports: [
     NewslettersModule,
     ArticlesModule,
-    JwtModule.register({
+    JwtModule.registerAsync({
+      inject: [ConfigService],
       global: true,
-      secret: process.env.JWT_SECRET_KEY,
-      signOptions: { expiresIn: '30d' },
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET_KEY'),
+        signOptions: { expiresIn: '30d' },
+      }),
     }),
   ],
   controllers: [UsersController],
