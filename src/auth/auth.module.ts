a3f3b65b21512eccf7a24ec5f2130152a26ff5fa
JAEYEONG KIM <kjyong702@naver.com>
2024-07-03 14:18:28 +0900

    비동기 이슈로 인해 registerAsync로 변경

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { ConfigService } from '@nestjs/config';
 import { TwilioModule } from 'nestjs-twilio';
 
 @Module({
   imports: [
-    TwilioModule.forRoot({
-      accountSid: process.env.TWILIO_ACCOUNT_SID,
-      authToken: process.env.TWILIO_AUTH_TOKEN,
+    TwilioModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        accountSid: config.get<string>('TWILIO_ACCOUNT_SID'),
+        authToken: config.get<string>('TWILIO_AUTH_TOKEN'),
+      }),
     }),
   ],
   controllers: [AuthController],
