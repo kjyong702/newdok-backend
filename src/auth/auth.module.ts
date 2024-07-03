b65fc5217a561c3436677b4c640e0b65ade8258a
JAEYEONG KIM <kjyong702@naver.com>
2024-07-03 15:47:49 +0900

    그냥 nestjs-twilio 라이브러리 안 쓸게....

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,21 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { ConfigModule } from '@nestjs/config';
-import { ConfigService } from '@nestjs/config';
-import { TwilioModule } from 'nestjs-twilio';
 
 @Module({
-  imports: [
-    TwilioModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (cfg: ConfigService) => ({
-        accountSid: cfg.get('TWILIO_ACCOUNT_SID'),
-        authToken: cfg.get('TWILIO_AUTH_TOKEN'),
-      }),
-      inject: [ConfigService],
-    }),
-  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
