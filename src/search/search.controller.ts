0dbab9b90c3bfd00b5eb5704f36104147ca4cca9
JAEYEONG KIM <kjyong702@gmail.com>
2025-03-06 19:19:24 +0900

    검색 기능(뉴스레터, 아티클) search 모듈로 이동

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,7 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { SearchService } from './search.service';
+import { ApiOperation, ApiQuery } from '@nestjs/swagger';
 
 @Controller('search')
 export class SearchController {
   constructor(private readonly searchService: SearchService) {}
+
+  @ApiOperation({
+    summary: '뉴스레터 브랜드 검색',
+    description: '검색어를 통해 뉴스레터 브랜드를 검색합니다.',
+  })
+  @ApiQuery({
+    name: 'brandName',
+    description: '검색어',
+    type: 'string',
+    example: 'NEWNEEK',
+  })
+  @Get('/newsletter')
+  async searchNewsletters(@Query('brandName') brandName: string) {
+    return await this.searchService.searchNewsletters(brandName);
+  }
+
+  @ApiOperation({
+    summary: '아티클 검색',
+    description: '검색어를 통해 아티클을 검색합니다.',
+  })
+  @ApiQuery({
+    name: 'keyword',
+    description: '검색어',
+    type: 'string',
+    example: '머니레터',
+  })
+  @Get('/article')
+  async searchArticles(@Query('keyword') keyword: string) {
+    return await this.searchService.searchArticles(keyword);
+  }
 }
